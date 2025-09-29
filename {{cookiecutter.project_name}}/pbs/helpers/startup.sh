8626baf79b829fcfdbdec1deee5c79e08ed6bfdf
Matteo Ranzi <matteoranzi2000@gmail.com>
2025-09-29 17:43:10 +0200

    Changed alias names

--- {{cookiecutter.project_name}}/pbs/helpers/startup.sh
+++ {{cookiecutter.project_name}}/pbs/helpers/startup.sh
@@ -3,6 +3,6 @@
 WORKING_DIRECTORY="/home/{{ cookiecutter.unitn_username }}/{{ cookiecutter.remote_project_path }}/{{ cookiecutter.project_name }}"
 cd "$WORKING_DIRECTORY" || exit
 
-alias lastOutput="./pbs/helpers/lastOutput.sh"
-alias lastError="./pbs/helpers/lastError.sh"
-alias lastStat="./pbs/helpers/lastStat.sh"
+alias lastout="./pbs/helpers/lastOutput.sh"
+alias lasterr="./pbs/helpers/lastError.sh"
+alias laststat="./pbs/helpers/lastStat.sh"
