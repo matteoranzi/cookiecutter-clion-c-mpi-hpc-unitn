c977d39e3b22ef8d2ec768e243c589bc2e9ed8ab
Matteo Ranzi <matteoranzi2000@gmail.com>
2025-10-17 14:48:35 +0200

    Fixed relative to absolute path hpc helper's startup script

--- {{cookiecutter.project_name}}/{{cookiecutter._scripts_dir}}/hpc_helpers/startup.sh
+++ {{cookiecutter.project_name}}/{{cookiecutter._scripts_dir}}/hpc_helpers/startup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 WORKING_DIRECTORY="/home/{{ cookiecutter.unitn_username }}/{{ cookiecutter.remote_project_path }}/{{ cookiecutter.project_name }}"
-cd "$WORKING_DIRECTORY" || exit
 
-alias lastout="./{{cookiecutter._scripts_dir}}/hpc_helpers/lastOutput.sh"
-alias lasterr="./{{cookiecutter._scripts_dir}}/hpc_helpers/lastError.sh"
-alias laststat="./{{cookiecutter._scripts_dir}}/hpc_helpers/lastStat.sh"
+alias lastout="${WORKING_DIRECTORY}/{{cookiecutter._scripts_dir}}/hpc_helpers/lastOutput.sh"
+alias lasterr="${WORKING_DIRECTORY}/{{cookiecutter._scripts_dir}}/hpc_helpers/lastError.sh"
+alias laststat="${WORKING_DIRECTORY}/{{cookiecutter._scripts_dir}}/hpc_helpers/lastStat.sh"
